d233634e933eaf66fa3d68afa3996cab7f17c7f8
manumanuameeen <jerrykerry3806@gmail.com>
2025-05-28 14:16:29 +0530

    google auth

--- src/components/firebase/firebase.tsx
+++ src/components/firebase/firebase.tsx
@@ -7,16 +7,16 @@ const firebaseConfig = {
   apiKey: 'AIzaSyATwz_SYDcUfI45HE4A-Jq1m4gNaQTt7e0',
   authDomain: 'olx-clone-35ce2.firebaseapp.com',
   projectId: 'olx-clone-35ce2',
-  storageBucket: 'olx-clone-35ce2.firebasestorage.app',
+  storageBucket: 'olx-clone-35ce2.appspot.com',
   messagingSenderId: '264556611038',
   appId: '1:264556611038:web:9971010e5f722595ec494f',
 };
-                                      
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-const storage = getStorage();
-const fireStore = getFirestore();
+const storage = getStorage(app);
+const fireStore = getFirestore(app);
 
 const fetchFromFireStore = async () => {
   try {
@@ -28,8 +28,8 @@ const fetchFromFireStore = async () => {
     }));
     console.log('Fetched products from Firestore:', productList);
     return productList;
-  } catch (error: any) {
-    console.error('Error fetching products from Firestore:', error.message);
+  } catch (error) {
+    console.error('Error fetching products from Firestore:', error);
     return [];
   }
 };
